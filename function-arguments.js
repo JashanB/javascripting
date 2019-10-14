c7f3aeb88797f87956142154bd97db2c0250abd0
Jashan <jashan_brar50@hotmail.com>
2019-10-14 21:48:58 +0000

    Initial commit with all my work so far

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math (s1, s2, s3) {
+  return (s2*s3) + s1
+}
+console.log(math(53, 61, 67))
---
+++ scope.js
@@ -0,0 +1,85 @@
+const a = 1; const b = 2; const c = 3;  
+       
+(function firstFunction () {  
+  const b = 5; const c = 6;  
+  
+  (function secondFunction () {  
+    const b = 8;  console.log(`a: ${a}, b: ${b}, c: ${c}`);
+  
+    (function thirdFunction () {  
+      const a = 7; const c = 9;  
+  
+      (function fourthFunction () {  
+        const a = 1; const c = 8  
+      })()  
+    })()  
+  })()  
+})()  
+To whom it may concern,
+
+I am applying for this position because I am passionate about health research and making a difference in people’s lives. I have over a year of relevant research experience where I performed tasks such as literature reviews, data extraction, evidence synthesis, editing and publishing manuscripts, analyzing patient data and liasoning with colleagues working on similar tasks to ensure a cohesive and comprehensive result. 
+
+In my last position I worked efficiently and effectively to update the Evidence Based Review of Stroke Rehabilitation database. This required conducting numerous systematic literature searches on different databases to extract all relevant Stroke Rehabilitation information and processing that data into an easily digestible format. Additionally, I wrote evidence based introductions and discussions for this database and provided strength of evidence summaries based on the quality of the evidence and data analysis. As part of this role I also conducted systematic reviews on other topics and processed this information into manuscripts that have been published in International Journals. I have presented my own posters at multiple Research Conferences throughout London, ON and Toronto, ON.  
+I have experience working with patients chart and was able to maintain the standard of professionalism and confidentiality expected when handling sensitive patient data. Through my degree in Biomedical Sciences and my previous experience working as a Research Associate, I have a strong understanding of medical terms and am able to quickly understand and grasp new information. I have strong interpersonal skills, written and verbal communicative skills and am extremely detail attentive. I also have strong technological skills and have in depth experience working with Microsoft Word, Excel and Powerpoint. I believe I would be a great fit for this position and I look forward to hearing back from you.
+
+Thank you for your consideration.
+
+Best Regards,
+
+Jashan Brar
+To whom it may concern,
+
+I am applying for this position because I am passionate about health research and making a difference in people’s lives. I have over a year of relevant research experience where I performed tasks such as literature reviews, data extraction, evidence synthesis, editing and publishing manuscripts, analyzing patient data and liasoning with colleagues working on similar tasks to ensure a cohesive and comprehensive result. 
+
+In my last position I worked efficiently and effectively to update the Evidence Based Review of Stroke Rehabilitation database. This required conducting numerous systematic literature searches on different databases to extract all relevant Stroke Rehabilitation information and processing that data into an easily digestible format. Additionally, I wrote evidence based introductions and discussions for this database and provided strength of evidence summaries based on the quality of the evidence and data analysis. As part of this role I also conducted systematic reviews on other topics and processed this information into manuscripts that have been published in International Journals. I have presented my own posters at multiple Research Conferences throughout London, ON and Toronto, ON.  
+I have experience working with patients chart and was able to maintain the standard of professionalism and confidentiality expected when handling sensitive patient data. Through my degree in Biomedical Sciences and my previous experience working as a Research Associate, I have a strong understanding of medical terms and am able to quickly understand and grasp new information. I have strong interpersonal skills, written and verbal communicative skills and am extremely detail attentive. I also have strong technological skills and have in depth experience working with Microsoft Word, Excel and Powerpoint. I believe I would be a great fit for this position and I look forward to hearing back from you.
+
+Thank you for your consideration.
+
+Best Regards,
+
+Jashan Brar To whom it may concern,
+
+I am applying for this position because I am passionate about health research and making a difference in people’s lives. I have over a year of relevant research experience where I performed tasks such as literature reviews, data extraction, evidence synthesis, editing and publishing manuscripts, analyzing patient data and liasoning with colleagues working on similar tasks to ensure a cohesive and comprehensive result. 
+
+In my last position I worked efficiently and effectively to update the Evidence Based Review of Stroke Rehabilitation database. This required conducting numerous systematic literature searches on different databases to extract all relevant Stroke Rehabilitation information and processing that data into an easily digestible format. Additionally, I wrote evidence based introductions and discussions for this database and provided strength of evidence summaries based on the quality of the evidence and data analysis. As part of this role I also conducted systematic reviews on other topics and processed this information into manuscripts that have been published in International Journals. I have presented my own posters at multiple Research Conferences throughout London, ON and Toronto, ON.  
+I have experience working with patients chart and was able to maintain the standard of professionalism and confidentiality expected when handling sensitive patient data. Through my degree in Biomedical Sciences and my previous experience working as a Research Associate, I have a strong understanding of medical terms and am able to quickly understand and grasp new information. I have strong interpersonal skills, written and verbal communicative skills and am extremely detail attentive. I also have strong technological skills and have in depth experience working with Microsoft Word, Excel and Powerpoint. I believe I would be a great fit for this position and I look forward to hearing back from you.
+
+Thank you for your consideration.
+
+Best Regards,
+
+Jashan Brar To whom it may concern,
+
+I am applying for this position because I am passionate about health research and making a difference in people’s lives. I have over a year of relevant research experience where I performed tasks such as literature reviews, data extraction, evidence synthesis, editing and publishing manuscripts, analyzing patient data and liasoning with colleagues working on similar tasks to ensure a cohesive and comprehensive result. 
+
+In my last position I worked efficiently and effectively to update the Evidence Based Review of Stroke Rehabilitation database. This required conducting numerous systematic literature searches on different databases to extract all relevant Stroke Rehabilitation information and processing that data into an easily digestible format. Additionally, I wrote evidence based introductions and discussions for this database and provided strength of evidence summaries based on the quality of the evidence and data analysis. As part of this role I also conducted systematic reviews on other topics and processed this information into manuscripts that have been published in International Journals. I have presented my own posters at multiple Research Conferences throughout London, ON and Toronto, ON.  
+I have experience working with patients chart and was able to maintain the standard of professionalism and confidentiality expected when handling sensitive patient data. Through my degree in Biomedical Sciences and my previous experience working as a Research Associate, I have a strong understanding of medical terms and am able to quickly understand and grasp new information. I have strong interpersonal skills, written and verbal communicative skills and am extremely detail attentive. I also have strong technological skills and have in depth experience working with Microsoft Word, Excel and Powerpoint. I believe I would be a great fit for this position and I look forward to hearing back from you.
+
+Thank you for your consideration.
+
+Best Regards,
+
+Jashan Brar To whom it may concern,
+
+I am applying for this position because I am passionate about health research and making a difference in people’s lives. I have over a year of relevant research experience where I performed tasks such as literature reviews, data extraction, evidence synthesis, editing and publishing manuscripts, analyzing patient data and liasoning with colleagues working on similar tasks to ensure a cohesive and comprehensive result. 
+
+In my last position I worked efficiently and effectively to update the Evidence Based Review of Stroke Rehabilitation database. This required conducting numerous systematic literature searches on different databases to extract all relevant Stroke Rehabilitation information and processing that data into an easily digestible format. Additionally, I wrote evidence based introductions and discussions for this database and provided strength of evidence summaries based on the quality of the evidence and data analysis. As part of this role I also conducted systematic reviews on other topics and processed this information into manuscripts that have been published in International Journals. I have presented my own posters at multiple Research Conferences throughout London, ON and Toronto, ON.  
+I have experience working with patients chart and was able to maintain the standard of professionalism and confidentiality expected when handling sensitive patient data. Through my degree in Biomedical Sciences and my previous experience working as a Research Associate, I have a strong understanding of medical terms and am able to quickly understand and grasp new information. I have strong interpersonal skills, written and verbal communicative skills and am extremely detail attentive. I also have strong technological skills and have in depth experience working with Microsoft Word, Excel and Powerpoint. I believe I would be a great fit for this position and I look forward to hearing back from you.
+
+Thank you for your consideration.
+
+Best Regards,
+
+Jashan Brar To whom it may concern,
+
+I am applying for this position because I am passionate about health research and making a difference in people’s lives. I have over a year of relevant research experience where I performed tasks such as literature reviews, data extraction, evidence synthesis, editing and publishing manuscripts, analyzing patient data and liasoning with colleagues working on similar tasks to ensure a cohesive and comprehensive result. 
+
+In my last position I worked efficiently and effectively to update the Evidence Based Review of Stroke Rehabilitation database. This required conducting numerous systematic literature searches on different databases to extract all relevant Stroke Rehabilitation information and processing that data into an easily digestible format. Additionally, I wrote evidence based introductions and discussions for this database and provided strength of evidence summaries based on the quality of the evidence and data analysis. As part of this role I also conducted systematic reviews on other topics and processed this information into manuscripts that have been published in International Journals. I have presented my own posters at multiple Research Conferences throughout London, ON and Toronto, ON.  
+I have experience working with patients chart and was able to maintain the standard of professionalism and confidentiality expected when handling sensitive patient data. Through my degree in Biomedical Sciences and my previous experience working as a Research Associate, I have a strong understanding of medical terms and am able to quickly understand and grasp new information. I have strong interpersonal skills, written and verbal communicative skills and am extremely detail attentive. I also have strong technological skills and have in depth experience working with Microsoft Word, Excel and Powerpoint. I believe I would be a great fit for this position and I look forward to hearing back from you.
+
+Thank you for your consideration.
+
+Best Regards,
+
+Jashan Brar2
